02b0309aa28a4cf46cecb9c72233f249c814849c
hamity1ld1r1m <117020299+hamity1ld1r1m@users.noreply.github.com>
2022-12-27 23:59:29 +0300

    update-app-package

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ cloudinary.config({
 conn();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 //ejs template engine
 app.set('view engine', 'ejs');
